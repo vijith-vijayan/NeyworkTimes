492998e56ffc53702163a24688a6dfe31782afee
vijith-vijayan <vijith047@gmail.com>
2022-04-02 11:34:33 +0530

    Remove unwanted code

--- NYTimes/ViewModel/ArticlesViewModel.swift
+++ NYTimes/ViewModel/ArticlesViewModel.swift
@@ -106,11 +106,3 @@ class ArticlesViewModel: ArticleViewModelProtocol {
         fetchArticles()
     }
 }
-
-
-extension Task where Success == Never, Failure == Never {
-    static func sleep(seconds: Double) async throws {
-        let duration = UInt64(seconds * 1_000_000_000)
-        try await Task.sleep(nanoseconds: duration)
-    }
-}
